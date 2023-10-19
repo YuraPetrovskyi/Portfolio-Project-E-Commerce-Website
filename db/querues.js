8dd04c9089bd72290f3f143d8808dc3ed4473e26
Yurii Petrovskyi <yurakarpaty@gmail.com>
2023-10-19 21:52:05 +0100

    created db

--- db/querues.js
+++ db/querues.js
@@ -33,14 +33,14 @@ DELETE: /users/:id | deleteUser()
 */
 
 // GET all users
-// const getUsers = (request, response) => {
-//   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
-//     if (error) {
-//       throw error
-//     }
-//     response.status(200).json(results.rows)
-//   })
-// }
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
 
 // // GET a single user by ID
 // const getUserById = (request, response) => {
