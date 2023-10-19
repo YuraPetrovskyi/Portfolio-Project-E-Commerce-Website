92b6058bbbf131356bdc8c794577ea929ad29ed1
Yurii Petrovskyi <yurakarpaty@gmail.com>
2023-10-19 16:02:33 +0100

    conected to a Posgres database and started to create db

---
+++ db/querues.js
@@ -0,0 +1,112 @@
+const Pool = require('pg').Pool
+
+
+const user = process.env.user;
+const password = process.env.password;
+
+const pool = new Pool({
+  user: user,
+  host: 'localhost',
+  database: 'e_commerce',
+  password: password,
+  port: 5432,
+})
+
+
+
+
+
+
+
+
+
+
+//  https://blog.logrocket.com/crud-rest-api-node-js-express-postgresql/
+
+/*
+GET: / | displayHome()
+GET: /users | getUsers()
+GET: /users/:id | getUserById()
+POST: /users | createUser()
+PUT: /users/:id | updateUser()
+DELETE: /users/:id | deleteUser()
+*/
+
+// GET all users
+// const getUsers = (request, response) => {
+//   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+
+// // GET a single user by ID
+// const getUserById = (request, response) => {
+//   const id = parseInt(request.params.id)
+
+//   pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).json(results.rows)
+//   })
+// }
+
+// // POST a new user
+// const createUser = (request, response) => {
+//   const { name, email } = request.body
+
+//   pool.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+//     if (error) {
+//       throw error
+//     } else if (!Array.isArray(results.rows) || results.rows.length < 1) {
+//     	throw error
+//     }
+//     response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+//   })
+// }
+
+// //  PUT updated data in an existing user
+// const updateUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+//   const { name, email } = request.body
+
+//   pool.query(
+//     'UPDATE users SET name = $1, email = $2 WHERE id = $3 RETURNING *',
+//     [name, email, id],
+//     (error, results) => {
+//       if (error) {
+//         throw error
+//       } 
+//       if (typeof results.rows == 'undefined') {
+//       	response.status(404).send(`Resource not found`);
+//       } else if (Array.isArray(results.rows) && results.rows.length < 1) {
+//       	response.status(404).send(`User not found`);
+//       } else {
+//         response.status(200).send(`User modified with ID: ${results.rows[0].id}`)         	
+//       }
+//     }
+//   )
+// }
+
+// // DELETE a user
+// const deleteUser = (request, response) => {
+//   const id = parseInt(request.params.id)
+
+//   pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+//     if (error) {
+//       throw error
+//     }
+//     response.status(200).send(`User deleted with ID: ${id}`)
+//   })
+// }
+
+// module.exports = {
+//   getUsers,
+//   getUserById,
+//   createUser,
+//   updateUser,
+//   deleteUser,
+// }
--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express()
 const port = 3000
 
 require('dotenv').config()
-// console.log(process.env)
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;  
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+const user = process.env.user;
+const password = process.env.password;
 
 app.use(bodyParser.json())
 app.use(
@@ -16,8 +18,36 @@ app.use(
   })
 )
 app.get('/', (request, response) => {
-  response.json({ info: GITHUB_CLIENT_ID })
+  response.json({ info: user + password })
 })
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+// const express = require('express')
+// const bodyParser = require('body-parser')  //the built-in body-parser middleware,
+// const app = express()
+// const port = 3000
+
+// const db = require('./queries')
+
+// app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
+
+// app.get('/', (request, response) => {
+//   response.json({ info: 'Node.js, Express, and Postgres API' })
+// })
+
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
+
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}.`)
+// })
